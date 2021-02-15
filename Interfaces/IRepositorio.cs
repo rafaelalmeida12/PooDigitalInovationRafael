c02c4950c3e990f433e84c8018c5d224d485d856
rafael almeida <rafaelgmrsa@gmail.com>
2021-02-15 19:48:35 -0400

    Subindo Projeto

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POODigitalInovationRafael.Interfaces
+{
+    public interface IRepositorio<T> where T : class
+    {
+        List<T> Lista();
+        T BuscarPorId(int Id);
+        void Adicionar(T entidade);
+        void Excluir(int Id);
+        void Atualizar(int Id, T entidade);
+        int ProximoId();
+    }
+}
---
+++ Models/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using POODigitalInovationRafael.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POODigitalInovationRafael.Models
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Adicionar(Serie obj)
+        {
+            listaSerie.Add(obj);
+        }
+
+        public void Atualizar(int Id, Serie entidade)
+        {
+            listaSerie[Id] = entidade;
+        }
+
+        public Serie BuscarPorId(int Id)
+        {
+            return listaSerie[Id];
+        }
+
+        public void Excluir(int Id)
+        {
+            listaSerie[Id].Excluir();
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+    }
+}
